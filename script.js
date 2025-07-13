8f812f50ff7c6ab5f7f649208a9651bc3a02e9c5
Connor7362 <Connorebauer@gmail.com>
2025-07-13 10:42:21 -0500

    Added randomizing colors button and functionality

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const whiteBackground = document.querySelector("#whiteBackground");
 const numericUpDown = document.querySelector("#numericUpDown");
+const btnRandom = document.querySelector("#random");
+btnRandom.style.filter = "brightness(50%)";
+const colors = ["red","blue","green","yellow","orange","purple","pink","brown","black"];
+let rows = 16;
+let randomColor = false;
 
 function createGrid(rows) {
-  //whiteBackground.innerHTML = '';
+  whiteBackground.innerHTML = ''; // this resets the html after 
 
   const squareSize = 720 / rows; // size in pixels
 
@@ -11,9 +16,14 @@ function createGrid(rows) {
     const square = document.createElement('div');
     square.style.width = squareSize + 'px';
     square.style.height = squareSize + 'px';
+    
 
     square.addEventListener('mouseenter', () => {
-      square.style.backgroundColor = 'black';
+        if(randomColor) square.style.backgroundColor = colors[Math.floor(Math.random() * 9)] ;
+        else square.style.backgroundColor = 'black';
+      
+      
+
     });
 
     whiteBackground.appendChild(square);
@@ -24,7 +34,23 @@ numericUpDown.addEventListener('input', (e) => {
   const value = e.target.value;
   if (value >= 1 && value <= 100) {
     createGrid(value);
+    rows = value;
   }
 });
+
+btnRandom.addEventListener('click', () => {
+    if(randomColor){
+        randomColor = false;
+        btnRandom.style.filter = "brightness(50%)";
+
+    }else{
+        randomColor = true;
+        btnRandom.style.filter = "";
+    }
+    
+
+});
+
+
 
 createGrid(16);
